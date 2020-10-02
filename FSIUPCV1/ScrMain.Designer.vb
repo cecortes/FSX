ff28aac579aedd86789bd080c338d23b7f6e6faa
César López Cortés <cesarlopezcortes@hotmail.com>
2020-10-01 19:50:01 -0500

    Basic conexion

--- FSIUPCV1/ScrMain.Designer.vb
+++ FSIUPCV1/ScrMain.Designer.vb
@@ -22,10 +22,86 @@ Partial Class ScrMain
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.BunifuDragControl1 = New Bunifu.Framework.UI.BunifuDragControl(Me.components)
+        Me.Barra = New System.Windows.Forms.TrackBar()
+        Me.LblEdo = New System.Windows.Forms.Label()
+        Me.TmrEvent = New System.Windows.Forms.Timer(Me.components)
+        Me.LblCon = New System.Windows.Forms.Label()
+        CType(Me.Barra, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BunifuDragControl1
+        '
+        Me.BunifuDragControl1.Fixed = True
+        Me.BunifuDragControl1.Horizontal = True
+        Me.BunifuDragControl1.TargetControl = Me
+        Me.BunifuDragControl1.Vertical = True
+        '
+        'Barra
+        '
+        Me.Barra.BackColor = System.Drawing.Color.Black
+        Me.Barra.LargeChange = 1
+        Me.Barra.Location = New System.Drawing.Point(12, 12)
+        Me.Barra.Maximum = 2
+        Me.Barra.Minimum = 1
+        Me.Barra.Name = "Barra"
+        Me.Barra.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.Barra.Size = New System.Drawing.Size(56, 104)
+        Me.Barra.TabIndex = 0
+        Me.Barra.TickStyle = System.Windows.Forms.TickStyle.Both
+        Me.Barra.Value = 1
+        '
+        'LblEdo
+        '
+        Me.LblEdo.AutoSize = True
+        Me.LblEdo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblEdo.ForeColor = System.Drawing.Color.Crimson
+        Me.LblEdo.Location = New System.Drawing.Point(134, 51)
+        Me.LblEdo.Name = "LblEdo"
+        Me.LblEdo.Size = New System.Drawing.Size(119, 22)
+        Me.LblEdo.TabIndex = 1
+        Me.LblEdo.Text = "Desconectado"
+        '
+        'TmrEvent
+        '
+        Me.TmrEvent.Interval = 50
+        '
+        'LblCon
+        '
+        Me.LblCon.AutoSize = True
+        Me.LblCon.BackColor = System.Drawing.Color.Black
+        Me.LblCon.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.LblCon.Location = New System.Drawing.Point(12, 153)
+        Me.LblCon.Name = "LblCon"
+        Me.LblCon.Size = New System.Drawing.Size(104, 20)
+        Me.LblCon.TabIndex = 2
+        Me.LblCon.Text = "Sin conexión"
+        '
+        'ScrMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(317, 289)
+        Me.Controls.Add(Me.LblCon)
+        Me.Controls.Add(Me.LblEdo)
+        Me.Controls.Add(Me.Barra)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "ScrMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Form1"
+        CType(Me.Barra, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents BunifuDragControl1 As Bunifu.Framework.UI.BunifuDragControl
+    Friend WithEvents Barra As TrackBar
+    Friend WithEvents LblEdo As Label
+    Friend WithEvents TmrEvent As Timer
+    Friend WithEvents LblCon As Label
 End Class
--- FSIUPCV1/ScrMain.vb
+++ FSIUPCV1/ScrMain.vb
@@ -1,3 +1,99 @@
-﻿Public Class ScrMain
+﻿'Imports
+Imports FSUIPC
+
+Public Class ScrMain
+
+#Region "Variables"
+
+#End Region
+
+#Region "Funciones"
+
+#End Region
+
+#Region "Métodos"
+
+    ''' <summary>
+    ''' Se encarga de mostrar el estado necesario del texto
+    ''' </summary>
+    Private Sub SetConexEdo()
+
+        'Actualiza las etiquetas de acuerdo al estado de la conexión
+        If FSUIPCConnection.IsOpen Then
+
+            'Open
+            LblEdo.ForeColor = Color.GreenYellow
+            LblEdo.Text = "Conectado"
+            LblCon.Text = "Conectado: " & FSUIPCConnection.FlightSimVersionConnected.ToString()
+            LblCon.ForeColor = Color.DeepSkyBlue
+
+        Else
+
+            'Close
+            LblEdo.ForeColor = Color.Red
+            LblEdo.Text = "Desconectado"
+            LblCon.Text = "Sin conexión"
+
+        End If
+    End Sub
+
+#End Region
+
+#Region "Eventos"
+
+    ''' <summary>
+    ''' Evalúa el valor de la barra para decidir conectar / desconectar
+    ''' Realiza las validaciones necesarias para abrir / cerrar la conexión
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Barra_ValueChanged(sender As Object, e As EventArgs) Handles Barra.ValueChanged
+
+        If Barra.Value > 1 Then
+
+            'Validación
+            If FSUIPCConnection.IsOpen Then
+
+                'Conexión previa abierta - Cerrar
+                TmrEvent.Stop()
+                FSUIPCConnection.Close()
+
+            Else
+
+                'Estado
+                LblEdo.ForeColor = Color.YellowGreen
+                LblEdo.Text = "Iniciando conexión..."
+
+                'Control de Errores
+                Try
+
+                    'Open
+                    FSUIPCConnection.Open()
+
+                Catch ex As FSUIPCException
+
+                    'Error
+                    MsgBox("Fallo de conexión..." & vbCrLf & vbCrLf & ex.Message)
+
+                End Try
+
+                'Validación
+                If FSUIPCConnection.IsOpen Then
+
+                    'Conexión abierta, iniciar el timer
+                    TmrEvent.Start()
+
+                End If
+
+            End If
+
+        End If
+
+        'Método de estado de la conexión
+        SetConexEdo()
+
+    End Sub
+
+#End Region
 
 End Class
