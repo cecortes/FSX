8a66c47e1043d50cf656537454eabdf0ad8e1339
César López Cortés <cesarlopezcortes@hotmail.com>
2020-10-02 19:38:43 -0500

    Offsets added

--- FSIUPCV1/ScrMain.Designer.vb
+++ FSIUPCV1/ScrMain.Designer.vb
@@ -28,14 +28,24 @@ Partial Class ScrMain
         Me.LblEdo = New System.Windows.Forms.Label()
         Me.TmrEvent = New System.Windows.Forms.Timer(Me.components)
         Me.LblCon = New System.Windows.Forms.Label()
+        Me.PnlTop = New System.Windows.Forms.Panel()
+        Me.PnlEdo = New System.Windows.Forms.Panel()
+        Me.LblAirSpeed = New System.Windows.Forms.Label()
+        Me.LblVS = New System.Windows.Forms.Label()
+        Me.PnlOffset = New System.Windows.Forms.Panel()
+        Me.LblHdg = New System.Windows.Forms.Label()
+        Me.LblGnd = New System.Windows.Forms.Label()
         CType(Me.Barra, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PnlTop.SuspendLayout()
+        Me.PnlEdo.SuspendLayout()
+        Me.PnlOffset.SuspendLayout()
         Me.SuspendLayout()
         '
         'BunifuDragControl1
         '
         Me.BunifuDragControl1.Fixed = True
         Me.BunifuDragControl1.Horizontal = True
-        Me.BunifuDragControl1.TargetControl = Me
+        Me.BunifuDragControl1.TargetControl = Me.PnlEdo
         Me.BunifuDragControl1.Vertical = True
         '
         'Barra
@@ -47,7 +57,7 @@ Partial Class ScrMain
         Me.Barra.Minimum = 1
         Me.Barra.Name = "Barra"
         Me.Barra.Orientation = System.Windows.Forms.Orientation.Vertical
-        Me.Barra.Size = New System.Drawing.Size(56, 104)
+        Me.Barra.Size = New System.Drawing.Size(56, 79)
         Me.Barra.TabIndex = 0
         Me.Barra.TickStyle = System.Windows.Forms.TickStyle.Both
         Me.Barra.Value = 1
@@ -57,7 +67,7 @@ Partial Class ScrMain
         Me.LblEdo.AutoSize = True
         Me.LblEdo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.LblEdo.ForeColor = System.Drawing.Color.Crimson
-        Me.LblEdo.Location = New System.Drawing.Point(134, 51)
+        Me.LblEdo.Location = New System.Drawing.Point(74, 9)
         Me.LblEdo.Name = "LblEdo"
         Me.LblEdo.Size = New System.Drawing.Size(119, 22)
         Me.LblEdo.TabIndex = 1
@@ -72,21 +82,101 @@ Partial Class ScrMain
         Me.LblCon.AutoSize = True
         Me.LblCon.BackColor = System.Drawing.Color.Black
         Me.LblCon.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.LblCon.Location = New System.Drawing.Point(12, 153)
+        Me.LblCon.Location = New System.Drawing.Point(74, 59)
         Me.LblCon.Name = "LblCon"
         Me.LblCon.Size = New System.Drawing.Size(104, 20)
         Me.LblCon.TabIndex = 2
         Me.LblCon.Text = "Sin conexión"
+        '
+        'PnlTop
+        '
+        Me.PnlTop.Controls.Add(Me.PnlOffset)
+        Me.PnlTop.Controls.Add(Me.PnlEdo)
+        Me.PnlTop.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PnlTop.Location = New System.Drawing.Point(0, 0)
+        Me.PnlTop.Name = "PnlTop"
+        Me.PnlTop.Size = New System.Drawing.Size(700, 92)
+        Me.PnlTop.TabIndex = 3
+        '
+        'PnlEdo
+        '
+        Me.PnlEdo.Controls.Add(Me.Barra)
+        Me.PnlEdo.Controls.Add(Me.LblCon)
+        Me.PnlEdo.Controls.Add(Me.LblEdo)
+        Me.PnlEdo.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PnlEdo.Location = New System.Drawing.Point(0, 0)
+        Me.PnlEdo.Name = "PnlEdo"
+        Me.PnlEdo.Size = New System.Drawing.Size(350, 92)
+        Me.PnlEdo.TabIndex = 0
+        '
+        'LblAirSpeed
+        '
+        Me.LblAirSpeed.AutoSize = True
+        Me.LblAirSpeed.BackColor = System.Drawing.Color.Black
+        Me.LblAirSpeed.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblAirSpeed.ForeColor = System.Drawing.Color.Orange
+        Me.LblAirSpeed.Location = New System.Drawing.Point(6, 9)
+        Me.LblAirSpeed.Name = "LblAirSpeed"
+        Me.LblAirSpeed.Size = New System.Drawing.Size(36, 24)
+        Me.LblAirSpeed.TabIndex = 1
+        Me.LblAirSpeed.Text = "--"
+        '
+        'LblVS
+        '
+        Me.LblVS.AutoSize = True
+        Me.LblVS.BackColor = System.Drawing.Color.Black
+        Me.LblVS.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblVS.ForeColor = System.Drawing.Color.Gold
+        Me.LblVS.Location = New System.Drawing.Point(6, 55)
+        Me.LblVS.Name = "LblVS"
+        Me.LblVS.Size = New System.Drawing.Size(36, 24)
+        Me.LblVS.TabIndex = 2
+        Me.LblVS.Text = "--"
+        '
+        'PnlOffset
+        '
+        Me.PnlOffset.Controls.Add(Me.LblGnd)
+        Me.PnlOffset.Controls.Add(Me.LblHdg)
+        Me.PnlOffset.Controls.Add(Me.LblVS)
+        Me.PnlOffset.Controls.Add(Me.LblAirSpeed)
+        Me.PnlOffset.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PnlOffset.Location = New System.Drawing.Point(350, 0)
+        Me.PnlOffset.Name = "PnlOffset"
+        Me.PnlOffset.Size = New System.Drawing.Size(320, 92)
+        Me.PnlOffset.TabIndex = 1
+        '
+        'LblHdg
+        '
+        Me.LblHdg.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblHdg.AutoSize = True
+        Me.LblHdg.BackColor = System.Drawing.Color.Black
+        Me.LblHdg.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblHdg.ForeColor = System.Drawing.Color.Yellow
+        Me.LblHdg.Location = New System.Drawing.Point(281, 9)
+        Me.LblHdg.Name = "LblHdg"
+        Me.LblHdg.Size = New System.Drawing.Size(36, 24)
+        Me.LblHdg.TabIndex = 3
+        Me.LblHdg.Text = "--"
+        '
+        'LblGnd
+        '
+        Me.LblGnd.AutoSize = True
+        Me.LblGnd.BackColor = System.Drawing.Color.Black
+        Me.LblGnd.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblGnd.ForeColor = System.Drawing.Color.GreenYellow
+        Me.LblGnd.Location = New System.Drawing.Point(153, 55)
+        Me.LblGnd.Name = "LblGnd"
+        Me.LblGnd.Size = New System.Drawing.Size(36, 24)
+        Me.LblGnd.TabIndex = 4
+        Me.LblGnd.Text = "--"
         '
         'ScrMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(317, 289)
-        Me.Controls.Add(Me.LblCon)
-        Me.Controls.Add(Me.LblEdo)
-        Me.Controls.Add(Me.Barra)
+        Me.ClientSize = New System.Drawing.Size(700, 100)
+        Me.Controls.Add(Me.PnlTop)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Margin = New System.Windows.Forms.Padding(4)
@@ -94,8 +184,12 @@ Partial Class ScrMain
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Form1"
         CType(Me.Barra, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PnlTop.ResumeLayout(False)
+        Me.PnlEdo.ResumeLayout(False)
+        Me.PnlEdo.PerformLayout()
+        Me.PnlOffset.ResumeLayout(False)
+        Me.PnlOffset.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -104,4 +198,11 @@ Partial Class ScrMain
     Friend WithEvents LblEdo As Label
     Friend WithEvents TmrEvent As Timer
     Friend WithEvents LblCon As Label
+    Friend WithEvents PnlTop As Panel
+    Friend WithEvents PnlEdo As Panel
+    Friend WithEvents LblAirSpeed As Label
+    Friend WithEvents LblVS As Label
+    Friend WithEvents PnlOffset As Panel
+    Friend WithEvents LblHdg As Label
+    Friend WithEvents LblGnd As Label
 End Class
--- FSIUPCV1/ScrMain.vb
+++ FSIUPCV1/ScrMain.vb
@@ -5,6 +5,19 @@ Public Class ScrMain
 
 #Region "Variables"
 
+    ''' <summary>
+    ''' Los offsets son variables y como tales se declaran al principio de la clase,
+    ''' pero nunca dentro de métodos.
+    ''' El tipo y tamaño del offset debe dde coincidir al tipo de dato almacenado en el.
+    ''' Para más información del tipo buscar en Basic Concepts de la UserGuide.PDF
+    ''' </summary>
+
+    Private airspeed As New Offset(Of UInteger)(&H2BC)                      ' 4-byte offset - Unsigned integer 
+    Private verticalSpeed As New Offset(Of Integer)(&H2C8)                  ' 4-byte offset - Signed integer 
+    Private compass As Offset(Of Double) = New Offset(Of Double)(&H2CC)     ' 8 byte offset - double (FLOAT64)
+    Private onGround As New Offset(Of UShort)(&H366)                        ' 2-byte offset - Unsigned short
+    Private aircraftType As New Offset(Of String)(&H3160, 24)               ' 24 byte offset - String
+
 #End Region
 
 #Region "Funciones"
@@ -35,6 +48,7 @@ Public Class ScrMain
             LblCon.Text = "Sin conexión"
 
         End If
+
     End Sub
 
 #End Region
@@ -87,10 +101,85 @@ Public Class ScrMain
 
             End If
 
+        Else
+
+            'Validación
+            If FSUIPCConnection.IsOpen Then
+
+                'Conexión previa abierta - Cerrar
+                TmrEvent.Stop()
+                FSUIPCConnection.Close()
+
+            End If
+
         End If
 
-        'Método de estado de la conexión
-        SetConexEdo()
+            'Método de estado de la conexión
+            SetConexEdo()
+
+    End Sub
+
+    ''' <summary>
+    ''' Obtiene los datos del FSUIPC por medio de la propiedad Process()
+    ''' Mediante la propiedad Value se accede a los datos del offset
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub TmrEvent_Tick(sender As Object, e As EventArgs) Handles TmrEvent.Tick
+
+        'Propiedad Process()
+        FSUIPCConnection.Process()
+
+        ' --------------------
+        ' AIRSPEED
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 4 bytes (UInteger) and holds the speed as knots * 128
+        ' So we need to convert back to knots by / 128
+        ' Offset is 'UInteger' so cast to double for conversion.
+
+        Dim airspeedKnots As Double = CDbl(airspeed.Value) / 128.0
+
+        ' Now the airspeed is in knots display in the text box with rounding to 2 dp.
+        LblAirSpeed.Text = airspeedKnots.ToString("F2") & " Knots"
+
+        ' --------------------
+        ' VERTICAL SPEED
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 4 bytes, signed (Integer) and holds the speed as metres/second * 256
+        ' We need to convert back to metres/second by / 256
+        ' Offset is 'Integer' so cast to double for conversion.
+
+        Dim vSpeedMPS As Double = CDbl(verticalSpeed.Value) / 256.0
+
+        ' If you want to display as feet/minute a further conversion is required:
+        Dim vSpeedFPM As Double = vSpeedMPS * 60.0 * 3.28084
+
+        ' Display one of these on the form (this time rounded to 0dp)
+        ' this.txtVerticalSpeed.Text = verticalSpeedMPS.ToString("F0");
+        LblVS.Text = vSpeedFPM.ToString("F0") & " Ft/min"
+
+        ' --------------------
+        ' COMPASS HEADING
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 8 bytes and holds a FLOAT64 (Double). The value is in degrees.
+        ' No conversion needed for this offset - Display directly on the form - rounded to 1dp.
+        LblHdg.Text = compass.Value.ToString("F1") & " Ø"
+
+        ' --------------------
+        ' ON GROUND
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 2 bytes (UShort). The value is either 1 (On ground) or 0 (Not on ground).
+        ' Create a string to display, based on the value
+        Dim onGroundText As String = If(onGround.Value > 0, "Yes", "No")
+        ' Display the text
+        LblGnd.Text = onGroundText & " " & aircraftType.Value
+
+        ' --------------------
+        ' Aircraft Type
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 24 bytes long and holds a string.
+        ' We use the string value directly to display on the form
+        'Me.txtAircraftType.Text = aircraftType.Value
 
     End Sub
 
