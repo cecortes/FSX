34536b863ef10115599750794df048b16c906512
César López Cortés <cesarlopezcortes@hotmail.com>
2020-10-03 20:09:21 -0500

    Write Offsets added

--- FSIUPCV1/ScrMain.Designer.vb
+++ FSIUPCV1/ScrMain.Designer.vb
@@ -24,20 +24,31 @@ Partial Class ScrMain
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.BunifuDragControl1 = New Bunifu.Framework.UI.BunifuDragControl(Me.components)
+        Me.PnlEdo = New System.Windows.Forms.Panel()
         Me.Barra = New System.Windows.Forms.TrackBar()
+        Me.LblCon = New System.Windows.Forms.Label()
         Me.LblEdo = New System.Windows.Forms.Label()
         Me.TmrEvent = New System.Windows.Forms.Timer(Me.components)
-        Me.LblCon = New System.Windows.Forms.Label()
         Me.PnlTop = New System.Windows.Forms.Panel()
-        Me.PnlEdo = New System.Windows.Forms.Panel()
-        Me.LblAirSpeed = New System.Windows.Forms.Label()
-        Me.LblVS = New System.Windows.Forms.Label()
+        Me.PnlAP = New System.Windows.Forms.Panel()
+        Me.SwPause = New Bunifu.Framework.UI.BunifuSwitch()
+        Me.LblPause = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LblAp = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SwAvionics = New Bunifu.Framework.UI.BunifuSwitch()
         Me.PnlOffset = New System.Windows.Forms.Panel()
-        Me.LblHdg = New System.Windows.Forms.Label()
         Me.LblGnd = New System.Windows.Forms.Label()
+        Me.LblHdg = New System.Windows.Forms.Label()
+        Me.LblVS = New System.Windows.Forms.Label()
+        Me.LblAirSpeed = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtPress = New System.Windows.Forms.TextBox()
+        Me.ChkPress = New System.Windows.Forms.CheckBox()
+        Me.PnlEdo.SuspendLayout()
         CType(Me.Barra, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PnlTop.SuspendLayout()
-        Me.PnlEdo.SuspendLayout()
+        Me.PnlAP.SuspendLayout()
         Me.PnlOffset.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -47,6 +58,17 @@ Partial Class ScrMain
         Me.BunifuDragControl1.Horizontal = True
         Me.BunifuDragControl1.TargetControl = Me.PnlEdo
         Me.BunifuDragControl1.Vertical = True
+        '
+        'PnlEdo
+        '
+        Me.PnlEdo.Controls.Add(Me.Barra)
+        Me.PnlEdo.Controls.Add(Me.LblCon)
+        Me.PnlEdo.Controls.Add(Me.LblEdo)
+        Me.PnlEdo.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PnlEdo.Location = New System.Drawing.Point(0, 0)
+        Me.PnlEdo.Name = "PnlEdo"
+        Me.PnlEdo.Size = New System.Drawing.Size(308, 92)
+        Me.PnlEdo.TabIndex = 0
         '
         'Barra
         '
@@ -61,6 +83,17 @@ Partial Class ScrMain
         Me.Barra.TabIndex = 0
         Me.Barra.TickStyle = System.Windows.Forms.TickStyle.Both
         Me.Barra.Value = 1
+        '
+        'LblCon
+        '
+        Me.LblCon.AutoSize = True
+        Me.LblCon.BackColor = System.Drawing.Color.Black
+        Me.LblCon.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.LblCon.Location = New System.Drawing.Point(74, 59)
+        Me.LblCon.Name = "LblCon"
+        Me.LblCon.Size = New System.Drawing.Size(104, 20)
+        Me.LblCon.TabIndex = 2
+        Me.LblCon.Text = "Sin conexión"
         '
         'LblEdo
         '
@@ -77,61 +110,105 @@ Partial Class ScrMain
         '
         Me.TmrEvent.Interval = 50
         '
-        'LblCon
-        '
-        Me.LblCon.AutoSize = True
-        Me.LblCon.BackColor = System.Drawing.Color.Black
-        Me.LblCon.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.LblCon.Location = New System.Drawing.Point(74, 59)
-        Me.LblCon.Name = "LblCon"
-        Me.LblCon.Size = New System.Drawing.Size(104, 20)
-        Me.LblCon.TabIndex = 2
-        Me.LblCon.Text = "Sin conexión"
-        '
         'PnlTop
         '
+        Me.PnlTop.Controls.Add(Me.PnlAP)
         Me.PnlTop.Controls.Add(Me.PnlOffset)
         Me.PnlTop.Controls.Add(Me.PnlEdo)
         Me.PnlTop.Dock = System.Windows.Forms.DockStyle.Top
         Me.PnlTop.Location = New System.Drawing.Point(0, 0)
         Me.PnlTop.Name = "PnlTop"
-        Me.PnlTop.Size = New System.Drawing.Size(700, 92)
+        Me.PnlTop.Size = New System.Drawing.Size(1024, 92)
         Me.PnlTop.TabIndex = 3
         '
-        'PnlEdo
+        'PnlAP
         '
-        Me.PnlEdo.Controls.Add(Me.Barra)
-        Me.PnlEdo.Controls.Add(Me.LblCon)
-        Me.PnlEdo.Controls.Add(Me.LblEdo)
-        Me.PnlEdo.Dock = System.Windows.Forms.DockStyle.Left
-        Me.PnlEdo.Location = New System.Drawing.Point(0, 0)
-        Me.PnlEdo.Name = "PnlEdo"
-        Me.PnlEdo.Size = New System.Drawing.Size(350, 92)
-        Me.PnlEdo.TabIndex = 0
+        Me.PnlAP.Controls.Add(Me.ChkPress)
+        Me.PnlAP.Controls.Add(Me.TxtPress)
+        Me.PnlAP.Controls.Add(Me.Label2)
+        Me.PnlAP.Controls.Add(Me.SwPause)
+        Me.PnlAP.Controls.Add(Me.LblPause)
+        Me.PnlAP.Controls.Add(Me.Label3)
+        Me.PnlAP.Controls.Add(Me.LblAp)
+        Me.PnlAP.Controls.Add(Me.Label1)
+        Me.PnlAP.Controls.Add(Me.SwAvionics)
+        Me.PnlAP.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PnlAP.Location = New System.Drawing.Point(582, 0)
+        Me.PnlAP.Name = "PnlAP"
+        Me.PnlAP.Size = New System.Drawing.Size(442, 92)
+        Me.PnlAP.TabIndex = 2
         '
-        'LblAirSpeed
+        'SwPause
         '
-        Me.LblAirSpeed.AutoSize = True
-        Me.LblAirSpeed.BackColor = System.Drawing.Color.Black
-        Me.LblAirSpeed.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblAirSpeed.ForeColor = System.Drawing.Color.Orange
-        Me.LblAirSpeed.Location = New System.Drawing.Point(6, 9)
-        Me.LblAirSpeed.Name = "LblAirSpeed"
-        Me.LblAirSpeed.Size = New System.Drawing.Size(36, 24)
-        Me.LblAirSpeed.TabIndex = 1
-        Me.LblAirSpeed.Text = "--"
+        Me.SwPause.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.SwPause.BorderRadius = 5
+        Me.SwPause.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.SwPause.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.SwPause.Location = New System.Drawing.Point(80, 40)
+        Me.SwPause.Margin = New System.Windows.Forms.Padding(6)
+        Me.SwPause.Name = "SwPause"
+        Me.SwPause.Oncolor = System.Drawing.Color.Red
+        Me.SwPause.Onoffcolor = System.Drawing.Color.GreenYellow
+        Me.SwPause.Size = New System.Drawing.Size(0, 0)
+        Me.SwPause.TabIndex = 5
+        Me.SwPause.Textcolor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.SwPause.Value = False
         '
-        'LblVS
+        'LblPause
         '
-        Me.LblVS.AutoSize = True
-        Me.LblVS.BackColor = System.Drawing.Color.Black
-        Me.LblVS.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblVS.ForeColor = System.Drawing.Color.Gold
-        Me.LblVS.Location = New System.Drawing.Point(6, 55)
-        Me.LblVS.Name = "LblVS"
-        Me.LblVS.Size = New System.Drawing.Size(36, 24)
-        Me.LblVS.TabIndex = 2
-        Me.LblVS.Text = "--"
+        Me.LblPause.AutoSize = True
+        Me.LblPause.ForeColor = System.Drawing.Color.GreenYellow
+        Me.LblPause.Location = New System.Drawing.Point(143, 40)
+        Me.LblPause.Name = "LblPause"
+        Me.LblPause.Size = New System.Drawing.Size(85, 20)
+        Me.LblPause.TabIndex = 4
+        Me.LblPause.Text = "Sin Pausa"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label3.Location = New System.Drawing.Point(6, 40)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Game"
+        '
+        'LblAp
+        '
+        Me.LblAp.AutoSize = True
+        Me.LblAp.ForeColor = System.Drawing.Color.Red
+        Me.LblAp.Location = New System.Drawing.Point(143, 9)
+        Me.LblAp.Name = "LblAp"
+        Me.LblAp.Size = New System.Drawing.Size(141, 20)
+        Me.LblAp.TabIndex = 2
+        Me.LblAp.Text = "AUTOPILOT OFF"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label1.Location = New System.Drawing.Point(6, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Avionics"
+        '
+        'SwAvionics
+        '
+        Me.SwAvionics.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.SwAvionics.BorderRadius = 5
+        Me.SwAvionics.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.SwAvionics.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.SwAvionics.Location = New System.Drawing.Point(80, 10)
+        Me.SwAvionics.Margin = New System.Windows.Forms.Padding(5)
+        Me.SwAvionics.Name = "SwAvionics"
+        Me.SwAvionics.Oncolor = System.Drawing.Color.GreenYellow
+        Me.SwAvionics.Onoffcolor = System.Drawing.Color.Red
+        Me.SwAvionics.Size = New System.Drawing.Size(0, 0)
+        Me.SwAvionics.TabIndex = 0
+        Me.SwAvionics.Textcolor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.SwAvionics.Value = False
         '
         'PnlOffset
         '
@@ -140,23 +217,10 @@ Partial Class ScrMain
         Me.PnlOffset.Controls.Add(Me.LblVS)
         Me.PnlOffset.Controls.Add(Me.LblAirSpeed)
         Me.PnlOffset.Dock = System.Windows.Forms.DockStyle.Left
-        Me.PnlOffset.Location = New System.Drawing.Point(350, 0)
+        Me.PnlOffset.Location = New System.Drawing.Point(308, 0)
         Me.PnlOffset.Name = "PnlOffset"
-        Me.PnlOffset.Size = New System.Drawing.Size(320, 92)
+        Me.PnlOffset.Size = New System.Drawing.Size(274, 92)
         Me.PnlOffset.TabIndex = 1
-        '
-        'LblHdg
-        '
-        Me.LblHdg.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.LblHdg.AutoSize = True
-        Me.LblHdg.BackColor = System.Drawing.Color.Black
-        Me.LblHdg.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblHdg.ForeColor = System.Drawing.Color.Yellow
-        Me.LblHdg.Location = New System.Drawing.Point(281, 9)
-        Me.LblHdg.Name = "LblHdg"
-        Me.LblHdg.Size = New System.Drawing.Size(36, 24)
-        Me.LblHdg.TabIndex = 3
-        Me.LblHdg.Text = "--"
         '
         'LblGnd
         '
@@ -170,12 +234,78 @@ Partial Class ScrMain
         Me.LblGnd.TabIndex = 4
         Me.LblGnd.Text = "--"
         '
+        'LblHdg
+        '
+        Me.LblHdg.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LblHdg.AutoSize = True
+        Me.LblHdg.BackColor = System.Drawing.Color.Black
+        Me.LblHdg.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblHdg.ForeColor = System.Drawing.Color.Yellow
+        Me.LblHdg.Location = New System.Drawing.Point(235, 9)
+        Me.LblHdg.Name = "LblHdg"
+        Me.LblHdg.Size = New System.Drawing.Size(36, 24)
+        Me.LblHdg.TabIndex = 3
+        Me.LblHdg.Text = "--"
+        '
+        'LblVS
+        '
+        Me.LblVS.AutoSize = True
+        Me.LblVS.BackColor = System.Drawing.Color.Black
+        Me.LblVS.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblVS.ForeColor = System.Drawing.Color.Gold
+        Me.LblVS.Location = New System.Drawing.Point(6, 55)
+        Me.LblVS.Name = "LblVS"
+        Me.LblVS.Size = New System.Drawing.Size(36, 24)
+        Me.LblVS.TabIndex = 2
+        Me.LblVS.Text = "--"
+        '
+        'LblAirSpeed
+        '
+        Me.LblAirSpeed.AutoSize = True
+        Me.LblAirSpeed.BackColor = System.Drawing.Color.Black
+        Me.LblAirSpeed.Font = New System.Drawing.Font("MS Gothic", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblAirSpeed.ForeColor = System.Drawing.Color.Orange
+        Me.LblAirSpeed.Location = New System.Drawing.Point(6, 9)
+        Me.LblAirSpeed.Name = "LblAirSpeed"
+        Me.LblAirSpeed.Size = New System.Drawing.Size(36, 24)
+        Me.LblAirSpeed.TabIndex = 1
+        Me.LblAirSpeed.Text = "--"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label2.Location = New System.Drawing.Point(6, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(195, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Altimeter Pressure (hPa)"
+        '
+        'TxtPress
+        '
+        Me.TxtPress.Location = New System.Drawing.Point(225, 67)
+        Me.TxtPress.Name = "TxtPress"
+        Me.TxtPress.Size = New System.Drawing.Size(71, 27)
+        Me.TxtPress.TabIndex = 7
+        '
+        'ChkPress
+        '
+        Me.ChkPress.Appearance = System.Windows.Forms.Appearance.Button
+        Me.ChkPress.AutoSize = True
+        Me.ChkPress.ForeColor = System.Drawing.Color.Firebrick
+        Me.ChkPress.Location = New System.Drawing.Point(313, 65)
+        Me.ChkPress.Name = "ChkPress"
+        Me.ChkPress.Size = New System.Drawing.Size(117, 30)
+        Me.ChkPress.TabIndex = 8
+        Me.ChkPress.Text = "Set Pressure"
+        Me.ChkPress.UseVisualStyleBackColor = True
+        '
         'ScrMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(700, 100)
+        Me.ClientSize = New System.Drawing.Size(1024, 100)
         Me.Controls.Add(Me.PnlTop)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -183,10 +313,13 @@ Partial Class ScrMain
         Me.Name = "ScrMain"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Form1"
-        CType(Me.Barra, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.PnlTop.ResumeLayout(False)
+        Me.TopMost = True
         Me.PnlEdo.ResumeLayout(False)
         Me.PnlEdo.PerformLayout()
+        CType(Me.Barra, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PnlTop.ResumeLayout(False)
+        Me.PnlAP.ResumeLayout(False)
+        Me.PnlAP.PerformLayout()
         Me.PnlOffset.ResumeLayout(False)
         Me.PnlOffset.PerformLayout()
         Me.ResumeLayout(False)
@@ -205,4 +338,14 @@ Partial Class ScrMain
     Friend WithEvents PnlOffset As Panel
     Friend WithEvents LblHdg As Label
     Friend WithEvents LblGnd As Label
+    Friend WithEvents PnlAP As Panel
+    Friend WithEvents SwAvionics As Bunifu.Framework.UI.BunifuSwitch
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LblAp As Label
+    Friend WithEvents SwPause As Bunifu.Framework.UI.BunifuSwitch
+    Friend WithEvents LblPause As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtPress As TextBox
+    Friend WithEvents ChkPress As CheckBox
 End Class
--- FSIUPCV1/ScrMain.vb
+++ FSIUPCV1/ScrMain.vb
@@ -12,11 +12,20 @@ Public Class ScrMain
     ''' Para más información del tipo buscar en Basic Concepts de la UserGuide.PDF
     ''' </summary>
 
+#Region "READ OFFSETS"
     Private airspeed As New Offset(Of UInteger)(&H2BC)                      ' 4-byte offset - Unsigned integer 
     Private verticalSpeed As New Offset(Of Integer)(&H2C8)                  ' 4-byte offset - Signed integer 
     Private compass As Offset(Of Double) = New Offset(Of Double)(&H2CC)     ' 8 byte offset - double (FLOAT64)
     Private onGround As New Offset(Of UShort)(&H366)                        ' 2-byte offset - Unsigned short
     Private aircraftType As New Offset(Of String)(&H3160, 24)               ' 24 byte offset - String
+#End Region
+
+#Region "WRITE OFFSETS"
+    Private avionicsMaster As New Offset(Of UInteger)(&H2E80)       ' 4-byte offset - Unsigned integer 
+    Private simPaused As New Offset(Of UShort)(&H264)               ' 2-byte offset - Unsigned Short  
+    Private pauseControl As New Offset(Of UShort)(&H262, True)      ' 2-byte offset - Unsigned Short     WRITE ONLY
+    Private altimeterPressure As New Offset(Of UShort)(&H330)       ' 2-byte offset - Unsigned Short
+#End Region
 
 #End Region
 
@@ -180,6 +189,134 @@ Public Class ScrMain
         ' FSUIPC Documentation says this offset is 24 bytes long and holds a string.
         ' We use the string value directly to display on the form
         'Me.txtAircraftType.Text = aircraftType.Value
+
+
+        ' Use the offsets declared to show the current state (values) to the user.
+        ' (Writes will be set up when the user clicks the buttons. See the 'click' event handlers.)
+
+        ' ----------------------
+        ' AVIONICS MASTER SWITCH
+        ' ----------------------
+        ' FSUIPC Documentation says this offset is 4 bytes (UInteger) and holds 0 (OFF) or 1 (ON) 
+        ' Show the button pressed in (checked) or not depending on the value we read
+        ' (Also the text colour is changed)
+        If avionicsMaster.Value = 1 Then
+
+            ' ON
+            'SwAvionics.Value = True
+            LblAp.ForeColor = Color.GreenYellow
+            LblAp.Text = "AUTOPILOT ON"
+
+        ElseIf Me.avionicsMaster.Value = 0 Then
+
+            ' OFF 
+            'SwAvionics.Value = False
+            LblAp.ForeColor = Color.Red
+            LblAp.Text = "AUTOPILOT OFF"
+
+        End If
+
+        ' --------------------
+        ' SIM PAUSED
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 2 bytes (UShort) and holds 0 (Not Paused) or 1 (Paused) 
+        ' Show the button pressed in (checked) or not depending on the value we read
+        ' (Also the text colour is changed)
+        If Me.simPaused.Value = 1 Then
+
+            ' Sim is Paused
+            LblPause.ForeColor = Color.Red
+            LblPause.Text = "Juego en Pausa"
+
+        ElseIf Me.simPaused.Value = 0 Then
+
+            ' Not paused
+            LblPause.ForeColor = Color.GreenYellow
+            LblPause.Text = "Sin Pausa"
+
+        End If
+
+        ' --------------------
+        ' ALTIMETER PRESSURE
+        ' --------------------
+        ' FSUIPC Documentation says this offset is 2 bytes (UShort). The value is in hectoPascals * 16
+        ' Need to convert this back to hectoPascals by / 16
+        Dim altimiterHPA As Double = CDbl(altimeterPressure.Value) / 16.0
+        ' Display on the form to 2dp
+        TxtPress.Text = altimiterHPA.ToString("F2")
+
+    End Sub
+
+    ''' <summary>
+    ''' Se encarga de activar o desactivar el piloto automático escribiendo el valor en el offset
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub SwAvionics_MouseClick(sender As Object, e As MouseEventArgs) Handles SwAvionics.MouseClick
+
+        'Validación
+        If SwAvionics.Value = True Then
+
+            'On AP OFFSET WRITE
+            ' User clicked switch ON
+            ' Set the Avionics offset value to 1 (meaning ON)
+            ' This will be sent the next time Process() is called. i.e. On the next timer tick.
+            avionicsMaster.Value = 1
+
+        Else
+
+            'OFF AP OFFSET WRITE
+            ' User clicked switch OFF
+            ' Set the Avionics offset value to 0 (meaning OFF)
+            ' This will be sent the next time Process() is called. i.e. On the next timer tick.
+            avionicsMaster.Value = 0
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Se encarga de poner el juego en pausa escribiendo el valor del offset
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub SwPause_MouseClick(sender As Object, e As MouseEventArgs) Handles SwPause.MouseClick
+
+        'Validación
+        If SwPause.Value = True Then
+
+            'Pause Game OFFSET WRITE
+            ' User clicked switch ON
+            ' Set the Pause Control offset value to 1 (meaning PAUSED)
+            ' This will be sent the next time Process() is called. i.e. On the next timer tick.
+            simPaused.Value = 1
+
+        Else
+
+            'Game Continue OFFSET WRITE
+            ' User clicked switch OFF
+            ' Set the Pause Control offset value to 0 (meaning NOT PAUSED)
+            ' This will be sent the next time Process() is called. i.e. On the next timer tick.
+            simPaused.Value = 0
+
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Modifica el valor de la presión standar
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub ChkPress_MouseClick(sender As Object, e As MouseEventArgs) Handles ChkPress.MouseClick
+
+        ' User has clicked the button to set standard pressure on the FORM
+        ' We will set the Altimeter Pressure Offset to 1013.25 (hPa)
+        ' The offset stores the value as hPa * 16, so we need to calculate this value
+        Dim pressureHPA As Double = 1013.25 * 16.0
+        ' Set the offset to this value (cast back to UShort)
+        altimeterPressure.Value = CUShort(Math.Truncate(pressureHPA))
+        ' This will be sent the next time Process() is called. i.e. On the next timer tick.
 
     End Sub
 
